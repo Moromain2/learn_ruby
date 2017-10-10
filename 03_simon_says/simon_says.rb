83d13b8e5eb47cf03aa80c69e9567aa02509c3a8
Romain Mosser <mosser.romain@gmail.com>
2017-10-10 21:57:42 +0200

    add 03-simon says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,5 +1,19 @@
 #write your code here
 
-def ("hello")
+def echo(greet)
+  if greet == "hello" || "bye"
+    "#{greet}"
+  end
+end
 
+def shout(shouting)
+  if shouting == "hello" || "hello world"
+    "#{shouting}".upcase
+  end
+end
+
+def repeat(message)
+  if message == "hello"
+    "#{message} #{message}"
+  end
 end
